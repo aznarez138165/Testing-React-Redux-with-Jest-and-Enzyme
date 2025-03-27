33a724f62f1ee930d3660c04909dcffd2ac39d2f
aznarez.138165 <aznarez.138165@gmail.com>
2025-03-27 13:15:14 +0100

    Cambio de logo en el header: añadido logo2.png y actualizado import

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import Logo from './../../assets/graphics/logo.png';
+import Logo from './../../assets/graphics/logo2.png';
 
 const Header = (props) => {
     return (
